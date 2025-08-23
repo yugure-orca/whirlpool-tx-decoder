bdd9b71ac43ad9da7222c060421c6e0a10ada366
yugure <109891005+yugure-orca@users.noreply.github.com>
2025-08-23 15:25:03 +0900

    add DTA test case (idempotent)

--- tests/dynamicTickArray.test.ts
+++ tests/dynamicTickArray.test.ts
@@ -15,7 +15,7 @@ describe("Dynamic Tick Array", () => {
     return { result: json.result.transactions.filter((tx: any) => tx.transaction.signatures[0] === signature)[0] };
   }
 
-  it("initializeDynamicTickArray", async () => {
+  it("initializeDynamicTickArray (idempotent = false)", async () => {
     const blockJSONFile = "initializeDynamicTickArray.json"
     const signature = "5qsohPWiHwYAeTkKgJUNDjPXy5pgKDEqsf7CZMMSat2LXhin7bibfyJYwJ9VEXzcMAWFbD4whxSS8sdGsm3qmVc6";
     const json = getTransactionJSON(blockJSONFile, signature);
@@ -33,5 +33,23 @@ describe("Dynamic Tick Array", () => {
     expect(ixs1.accounts.tickArray).toEqual("EHtXA1XLXstmpfvUimpY82x8XLpJxat9hyBUvmu7EoVy");
     expect(ixs1.accounts.systemProgram).toEqual("11111111111111111111111111111111");
   });
-  
+
+  it("initializeDynamicTickArray (idempotent = true)", async () => {
+    const blockJSONFile = "initializeDynamicTickArrayIdempotent.json"
+    const signature = "srwbAy7Jrs7Ueso9qYJ8mA1vi9LuWVxVNtttqLEyp7aqafoy9pYJVTE3QCrqfPuAxXrvy6jXNuQ7uHayrhnYsvN";
+    const json = getTransactionJSON(blockJSONFile, signature);
+
+    const ixs = WhirlpoolTransactionDecoder.decode(json, WHIRLPOOL_PROGRAM_ID);
+    expect(ixs.length).toEqual(1);
+    expect(ixs[0].name).toEqual("initializeDynamicTickArray");
+
+    const ixs0 = ixs[0] as DecodedInitializeDynamicTickArrayInstruction;
+
+    expect(ixs0.data.startTickIndex).toEqual(-157696);
+    expect(ixs0.data.idempotent).toEqual(true);
+    expect(ixs0.accounts.whirlpool).toEqual("HzyWBmNT6in1VxxwBziJtGNmCu8jAVh3jnS8a8r8HMfB");
+    expect(ixs0.accounts.funder).toEqual("r21Gamwd9DtyjHeGywsneoQYR39C1VDwrw7tWxHAwh6");
+    expect(ixs0.accounts.tickArray).toEqual("2kc1Anp59JD9dbPmyVqZuXHkT8JBQn9kBkhTe6QLZjCh");
+    expect(ixs0.accounts.systemProgram).toEqual("11111111111111111111111111111111");
+  });
 });
